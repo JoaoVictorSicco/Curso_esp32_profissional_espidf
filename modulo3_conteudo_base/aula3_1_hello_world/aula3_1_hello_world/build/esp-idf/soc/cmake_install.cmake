068697af4b6efc4728fe8037798e64e105852621
JoaoVictorSicco <victordesicco@gmail.com>
2023-06-08 19:43:53 -0400

    Ex01 - Hello World na esp-idf

---
+++ modulo3_conteudo_base/aula3_1_hello_world/aula3_1_hello_world/build/esp-idf/soc/cmake_install.cmake
@@ -0,0 +1,40 @@
+# Install script for directory: C:/Users/victo/esp/esp-idf/components/soc
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/aula3_1_hello_world")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "TRUE")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("E:/git_repositorio/Curso_esp32_profissional_espidf/modulo3_conteudo_base/aula3_1_hello_world/aula3_1_hello_world/build/esp-idf/soc/esp32/cmake_install.cmake")
+
+endif()
+
