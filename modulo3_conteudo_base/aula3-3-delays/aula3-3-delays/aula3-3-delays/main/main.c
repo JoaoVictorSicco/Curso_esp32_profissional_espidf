7c24b1cb28b96c67408dc076f08d7b975736ffde
JoaoVictorSicco <victordesicco@gmail.com>
2023-06-09 12:02:52 -0400

    Ex03 - Delays na esp-idf

---
+++ modulo3_conteudo_base/aula3-3-delays/aula3-3-delays/aula3-3-delays/main/main.c
@@ -0,0 +1,41 @@
+/* Hello World Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include "driver/gpio.h"
+
+#define PIN 13
+
+void app_main(void)
+{
+    // printf("Hello world!\n");
+    gpio_pad_select_gpio(PIN);                          // Seleciona o pino
+    bool status = 0;
+
+    while (1)
+    {
+        status = !status;
+        gpio_set_level(PIN,status);                     // Alterna o estado do pino em HIGH ou LOW
+        printf("Hello world!\n");
+        vTaskDelay(1000/portTICK_PERIOD_MS);            // Delay de 1s
+        for (int i = 1;i<11;i++)
+        {
+            printf("count %d\n",i);
+            vTaskDelay(1000/portTICK_PERIOD_MS);
+        }
+
+    }
+    
+
+    
+}
